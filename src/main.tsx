88618dece889e82c4397ec3aa97428523c02f09c
shangobashi <eloubw@gmail.com>
2025-06-26 15:00:35 +0200

    fix: switch to HashRouter for GitHub Pages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './lib/fonts.css';
 import './index.css';
@@ -9,11 +9,11 @@ import { AuthProvider } from './contexts/auth-context';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
         <Toaster />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
